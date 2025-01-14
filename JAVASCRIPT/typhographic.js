1a2339ea64ade3b2e70f251b61c7a5eae54cc95b
Lungile-Tshatshu <LungileLolwane025@gmail.com>
2025-01-14 10:34:09 +0200

    update

--- JAVASCRIPT/typhographic.js
+++ JAVASCRIPT/typhographic.js
@@ -1,10 +1,7 @@
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function getRandomLetter() {
- var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
- return alphabet[rand(0,alphabet.length - 1)]
-}
+
 function getRandomWord(word) {
   var text = word.innerHTML
   
